12569bea3ebfe779d3f50228cdf17af6749b30d8
kotan2 <stvn.frst@gmail.com>
2025-02-18 23:48:22 +0300

    to update

---
+++ Topics/Quotes and multi-line strings/Examples/main.py
@@ -0,0 +1,15 @@
+#  You can experiment here, it won’t be checked
+
+# example 1
+print("You're doing great!")
+print('You\'re doing great!')
+
+# example 2
+print("Have you read \"Hamlet\"?")
+print('Have you read "Hamlet"?')
+
+print("""This
+is
+a
+multi-line
+string""")
---
+++ Topics/Quotes and multi-line strings/Quotation quandary  A chat conundrum/main.py
@@ -0,0 +1,6 @@
+#  You can experiment here, it won’t be checked
+dialogue = '''Alice said, "I 'm not sure about this."
+Bob replied, 'Why don\'t we try it?'
+Alice: "Okay, let's give it a shot!"
+'''
+print(dialogue)
